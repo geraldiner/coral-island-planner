19c601fdb94b4bb201cf9ff85080b27be7c354e0
Geraldine R <hello@geraldiner.com>
2023-01-22 14:53:53 -0500

    upload buildable areas json

--- public/js/data/generate_regular_data.js
+++ public/js/data/generate_regular_data.js
@@ -1,53 +1,122 @@
 const fs = require('fs');
 
-const tillableFilePath = './public/js/data/regular_tillable.json';
+const tiledMapDataFilePath = './public/js/data/coral_island_planner.json';
+const tillableOutputFilePath = './public/js/data/regular_tillable.json';
+const buildableOutputFilePath = './public/js/data/regular_buildable.json';
 const TILE_SIZE = 16;
+const TILED_LAYER_KEYS = {
+  BUILDABLE: 'Buildable area',
+  TILLABLE: 'Tillable area (field)',
+  NON_BUILDABLE_WATER: 'Non-Buildable area (water)',
+  NON_BUILDABLE_GRASS: 'Non-Buildable area (grass)',
+  NON_BUILDABLE_BUILDING: 'Non-Buildable area (building)',
+};
+const TILED_WIDTH = 90;
+const TILED_HEIGHT = 72;
 
-const tillableTiles = {};
-const tillablePositions = {};
+const makeDeepCopy = (objToCopy) => JSON.parse(JSON.stringify(objToCopy));
 
-const generateTillableTilesAndPositions = (start, end, tileSize) => {
-  for (let { x } = start; x <= end.x; x += tileSize) {
-    for (let { y } = start; y <= end.y; y += tileSize) {
-      const tileX = x / tileSize;
-      const tileY = y / tileSize;
-      if (!tillableTiles[tileX]) {
-        tillableTiles[tileX] = [];
-      }
-      tillableTiles[tileX].push(tileY);
-      if (!tillablePositions[x]) {
-        tillablePositions[x] = [];
-      }
-      tillablePositions[x].push(y);
+const writeToFile = (filePath, dataToWrite) => {
+  fs.writeFile(filePath, JSON.stringify(dataToWrite), (error) => {
+    if (error) {
+      throw error;
     }
-  }
-};
-const tillableStart = { x: 112, y: 192 };
-const tillableEnd = { x: 1184, y: 832 };
-generateTillableTilesAndPositions(tillableStart, tillableEnd, TILE_SIZE);
-
-const tillableSectionStart = { x: 512, y: 848 };
-const tillableSectionEnd = { x: 1184, y: 1120 };
-generateTillableTilesAndPositions(
-  tillableSectionStart,
-  tillableSectionEnd,
-  TILE_SIZE,
-);
-const tillableSectionStart2 = { x: 1200, y: 656 };
-const tillableSectionEnd2 = { x: 1312, y: 1120 };
-generateTillableTilesAndPositions(
-  tillableSectionStart2,
-  tillableSectionEnd2,
-  TILE_SIZE,
-);
-
-const data = {
-  tiles: tillableTiles,
-  positions: tillablePositions,
+  });
 };
 
-fs.writeFile(tillableFilePath, JSON.stringify(data), (error) => {
-  if (error) {
-    throw error;
+const generateTillableTilesAndPositionsJson = (writeDataToFile = false) => {
+  const tillableTiles = {};
+  const tillablePositions = {};
+  const generateTillableTilesAndPositions = (start, end, tileSize) => {
+    for (let { x } = start; x <= end.x; x += tileSize) {
+      for (let { y } = start; y <= end.y; y += tileSize) {
+        const tileX = x / tileSize;
+        const tileY = y / tileSize;
+        if (!tillableTiles[tileX]) {
+          tillableTiles[tileX] = [];
+        }
+        tillableTiles[tileX].push(tileY);
+        if (!tillablePositions[x]) {
+          tillablePositions[x] = [];
+        }
+        tillablePositions[x].push(y);
+      }
+    }
+  };
+  const tillableStart = { x: 112, y: 192 };
+  const tillableEnd = { x: 1184, y: 832 };
+  generateTillableTilesAndPositions(tillableStart, tillableEnd, TILE_SIZE);
+
+  const tillableSectionStart = { x: 512, y: 848 };
+  const tillableSectionEnd = { x: 1184, y: 1120 };
+  generateTillableTilesAndPositions(
+    tillableSectionStart,
+    tillableSectionEnd,
+    TILE_SIZE,
+  );
+  const tillableSectionStart2 = { x: 1200, y: 656 };
+  const tillableSectionEnd2 = { x: 1312, y: 1120 };
+  generateTillableTilesAndPositions(
+    tillableSectionStart2,
+    tillableSectionEnd2,
+    TILE_SIZE,
+  );
+
+  const tillableData = {
+    tiles: tillableTiles,
+    positions: tillablePositions,
+  };
+
+  if (writeDataToFile) {
+    writeToFile(tillableOutputFilePath, tillableData);
   }
-});
+  return tillableData;
+};
+
+const tillableData = generateTillableTilesAndPositionsJson();
+
+const generateBuildableTilesAndPositionsJson = (writeDataToFile = false) => {
+  const buildableTiles = makeDeepCopy(tillableData.tiles);
+  const buildablePositions = makeDeepCopy(tillableData.positions);
+  fs.readFile(tiledMapDataFilePath, (err, json) => {
+    if (err) {
+      throw err;
+    }
+    const data = JSON.parse(json);
+    const { layers } = data;
+    const layersDict = layers.reduce((prevDict, layer) => {
+      prevDict[layer.name] = layer;
+      return prevDict;
+    }, {});
+    const buildableLayer = layersDict[TILED_LAYER_KEYS.BUILDABLE];
+    const buildableDataTiles = buildableLayer.data;
+    console.log(buildableDataTiles);
+    for (let tile = 0; tile < buildableDataTiles.length; tile++) {
+      if (buildableDataTiles[tile] > 0) {
+        console.log(tile);
+        const tileY = Math.floor(tile / TILED_WIDTH);
+        const tileX = tile % TILED_WIDTH;
+        const positionX = tileX * TILE_SIZE;
+        const positionY = tileY * TILE_SIZE;
+        if (!buildableTiles[tileX]) {
+          buildableTiles[tileX] = [];
+        }
+        buildableTiles[tileX].push(tileY);
+        if (!buildablePositions[positionX]) {
+          buildablePositions[positionX] = [];
+        }
+        buildablePositions[positionX].push(positionY);
+      }
+    }
+    const buildableData = {
+      tiles: buildableTiles,
+      positions: buildablePositions,
+    };
+    if (writeDataToFile) {
+      writeToFile(buildableOutputFilePath, buildableData);
+    }
+    return buildableData;
+  });
+};
+
+const buildableData = generateBuildableTilesAndPositionsJson(true);
--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,15 @@
 const svgNamespace = 'http://www.w3.org/2000/svg';
 const TILE_SIZE = 16;
+let TILLABLE_DATA;
+let BUILDABLE_DATA;
+
 // variables to keep track of
 const currentSprite = 'crop';
+const isCrop = true;
 const objectTiles = {};
 const isDrag = false;
-let TILLABLE_DATA;
 
-const plannerCanvasSvg = document.querySelector('#planner-canvas__svg');
-
+// Fetch json data for restriction checking
 const fetchJsonData = async (uri) => {
   try {
     const response = await fetch(uri);
@@ -53,41 +55,47 @@ const createSvgElementWithAttributes = (
   fill = null,
   style = null,
   href = null,
+  textContent = null,
   id = null,
   classes = null,
 ) => {
   const svgElement = document.createElementNS(svgNamespace, svgType);
-  svgElement.setAttributeNS(null, 'x', x);
-  svgElement.setAttributeNS(null, 'y', y);
-  svgElement.setAttributeNS(null, 'width', width);
-  svgElement.setAttributeNS(null, 'height', height);
+  svgElement.setAttribute('x', x);
+  svgElement.setAttribute('y', y);
+  svgElement.setAttribute('width', width);
+  svgElement.setAttribute('height', height);
   if (fill) {
-    svgElement.setAttributeNS(null, 'fill', fill);
+    svgElement.setAttribute('fill', fill);
   }
   if (style) {
-    svgElement.setAttributeNS(null, 'style', style);
+    svgElement.setAttribute('style', style);
   }
   if (href) {
-    svgElement.setAttributeNS(null, 'href', href);
+    svgElement.setAttribute('href', href);
   }
-
+  if (textContent) {
+    svgElement.textContent = textContent;
+  }
   if (id) {
-    svgElement.setAttributeNS(null, 'id', id);
+    svgElement.setAttribute('id', id);
   }
   if (classes) {
-    svgElement.setAttributeNS(null, 'class', classes);
+    svgElement.setAttribute('class', classes);
   }
   return svgElement;
 };
 
-const setupListeners = () => {
+const setupEditorListeners = () => {
+  const plannerCanvasSvg = document.querySelector('#planner-canvas__svg');
+
   // Follow the cursor movement
   plannerCanvasSvg.addEventListener('mousemove', (e) => {
     const normalizedPosition = normalizePositionWithSnap(e, null, TILE_SIZE);
-    console.log(normalizedPosition);
     const oldPointer = document.getElementById('pointer');
+    const oldPointerText = document.getElementById('pointer-text');
     if (oldPointer) {
       oldPointer.remove();
+      oldPointerText.remove();
     }
     const isTillableTile = isTillable(
       normalizedPosition.x,
@@ -107,9 +115,24 @@ const setupListeners = () => {
       fill,
       'opacity: 0.5;',
       href,
+      null,
       'pointer',
     );
+    const text = createSvgElementWithAttributes(
+      'text',
+      normalizedPosition.x,
+      normalizedPosition.y,
+      TILE_SIZE * 5,
+      TILE_SIZE,
+      '#fff',
+      null,
+      null,
+      `X: ${normalizedPosition.x}, Y: ${normalizedPosition.y} | 
+      Tile: ${normalizedPosition.x / 16}, ${normalizedPosition.y / 16}`,
+      'pointer-text',
+    );
     plannerCanvasSvg.appendChild(pointer);
+    plannerCanvasSvg.appendChild(text);
   });
 
   // Place current sprite
@@ -121,8 +144,8 @@ const setupListeners = () => {
         return;
       }
       if (
-        !objectTiles[normalizedPosition.x]
-        || !objectTiles[normalizedPosition.x].includes(normalizedPosition.y)
+        !objectTiles[normalizedPosition.x] ||
+        !objectTiles[normalizedPosition.x].includes(normalizedPosition.y)
       ) {
         if (!objectTiles[normalizedPosition.x]) {
           objectTiles[normalizedPosition.x] = [];
@@ -146,11 +169,24 @@ const setupListeners = () => {
   });
 };
 
+const setupFormListeners = () => {
+  const editorMenu = document.getElementById('editor-menu');
+  const farmEquipmentSelect = document.getElementById('farm-equipment-select');
+
+  farmEquipmentSelect.onchange = (e) => {
+    console.log(e);
+  };
+};
+
 const getJsonData = async () => {
   TILLABLE_DATA = await fetchJsonData(
     'https://raw.githubusercontent.com/geraldiner/coral-island-planner/main/public/js/data/regular_tillable.json',
   );
-  setupListeners();
+  BUILDABLE_DATA = await fetchJsonData(
+    'https://raw.githubusercontent.com/geraldiner/coral-island-planner/main/public/js/data/regular_buildable.json',
+  );
+  setupEditorListeners();
+  setupFormListeners();
 };
 window.addEventListener('load', () => {
   console.log('window onload says hi');
